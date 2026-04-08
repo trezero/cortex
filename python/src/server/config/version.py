a6ebfe3b423450e141d503e5415dcd324bdf851a
Jason Perr <trezero@gmail.com>
2026-04-08 11:23:16 -0700

    feat: add is_default column to archon_extensions

--- python/src/server/config/version.py
+++ python/src/server/config/version.py
@@ -4,8 +4,8 @@ Version configuration for Archon.
 
 # Current version of Archon
 # Update this with each release
-ARCHON_VERSION = "0.1.0"
+ARCHON_VERSION = "1.0.0"
 
 # Repository information for GitHub API
-GITHUB_REPO_OWNER = "coleam00"
+GITHUB_REPO_OWNER = "trezero"
 GITHUB_REPO_NAME = "Archon"
